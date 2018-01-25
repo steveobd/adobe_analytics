b43f1e392f5977d4e23ca3e2af3b0d2fdea7127a
Martin Winkel <martin.winkel.pps@gmail.com>
2018-01-24 23:00:55 -0800

    updated version string

--- adobe_analytics/__init__.py
+++ adobe_analytics/__init__.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import os
 
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 app_dir = os.path.dirname(__file__)
 app_dir_components = app_dir.split(os.sep)
 base_dir = os.sep.join(app_dir_components[:-1])
